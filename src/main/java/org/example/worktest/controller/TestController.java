d3e09761f3a285200a9774ac6c82442ea36f67ef
jdragon <jdraong9527@gmail.com>
2025-01-02 16:05:38 +0800

    feat: sse测试

--- src/main/java/org/example/worktest/controller/TestController.java
+++ src/main/java/org/example/worktest/controller/TestController.java
@@ -3,11 +3,15 @@ package org.example.worktest.controller;
 
 import org.example.worktest.entity.Order;
 import org.example.worktest.service.OrderService;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyEmitter;
 
+import java.util.Date;
 import java.util.List;
+import java.util.concurrent.CompletableFuture;
 
 @RestController
 @RequestMapping("/test")
@@ -22,5 +26,29 @@ public class TestController {
     @GetMapping
     public List<Order> listOrder() {
         return orderService.list();
+    }
+
+    @GetMapping("/bodyEmitter")
+    public ResponseBodyEmitter handle() {
+        // 创建一个ResponseBodyEmitter，-1代表不超时
+        ResponseBodyEmitter emitter = new ResponseBodyEmitter(-1L);
+        // 异步执行耗时操作
+        CompletableFuture.runAsync(() -> {
+            try {
+                // 模拟耗时操作
+                for (int i = 0; i < 10000; i++) {
+                    System.out.println("bodyEmitter " + i);
+                    // 发送数据
+                    emitter.send("bodyEmitter " + i + " @ " + new Date() + "\n");
+                    Thread.sleep(2000);
+                }
+                // 完成
+                emitter.complete();
+            } catch (Exception e) {
+                // 发生异常时结束接口
+                emitter.completeWithError(e);
+            }
+        });
+        return emitter;
     }
 }
